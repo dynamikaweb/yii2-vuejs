755c40557c333b85b035a38d353a55b91a465536
RodrigoDornelles <rodrigo@dornelles.me>
2021-08-16 14:03:36 -0300

    Create VueAssetCDN.php

---
+++ src/VueAssetCDN.php
@@ -0,0 +1,11 @@
+  
+<?php
+
+namespace dynamikaweb\vuejs;
+
+class VueAssetCDN extends \yii\web\AssetBundle
+{
+    public $js = [
+        '//cdn.jsdelivr.net/npm/vue@2/dist/vue.min.js',
+    ];
+}
